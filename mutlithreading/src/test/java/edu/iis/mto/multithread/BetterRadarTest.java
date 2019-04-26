3a11228d210ed06df4fb384656cd25e0d5c1e128
rogo6 <piotrrogozinski95@gmail.com>
2019-04-26 16:08:46 +0200

    Added repeat rule in better radar test

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,12 +1,19 @@
 package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
+
+import edu.iis.mto.multithread.RepeatRule.Repeat;
 
 public class BetterRadarTest {
 	
+	@Rule
+	public RepeatRule repeatRule = new RepeatRule();
+	
+	@Repeat(times = 20)
 	@Test
 	public void launchPatriotSpecificAmountOfTimesWhenNoticesEnemyMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
