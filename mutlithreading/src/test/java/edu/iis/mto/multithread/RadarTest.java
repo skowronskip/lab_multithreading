585f0143e66d6dcfec5331073c7cd2a9b2e727bc
mcfb <maciekfab@gmail.com>
2019-05-02 18:50:49 +0200

    add ReapetRule for repeatedly run some tests

--- mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/RadarTest.java
@@ -3,11 +3,18 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+import org.junit.Rule;
 import org.junit.Test;
+
+
 
 public class RadarTest {
 
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     @Test
+    @RepeatRule.Repeat(times = 10 )
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         Radar radar = new Radar(batteryMock);
