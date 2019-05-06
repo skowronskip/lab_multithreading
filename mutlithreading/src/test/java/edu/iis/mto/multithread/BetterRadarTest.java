91335a7a8419000594d97c0b599d093bb862208e
Adam Sylla <sylla.adam.97@gmail.com>
2019-05-06 12:25:00 +0200

    *added RepeatRule class *added repetiotion of test

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,23 +1,24 @@
 package edu.iis.mto.multithread;
 
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.Mockito.*;
 
 public class BetterRadarTest {
 
-    PatriotBattery batteryMock;
+    private static PatriotBattery batteryMock;
 
-    @Before
-    public void setup(){
-        batteryMock = mock(PatriotBattery.class);
-    }
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
 
-    @Test
+    @RepeatedTest(1000)
     public void patriotsShouldBeLaunchedGivenAmountTimes(){
-
-        BetterRadar betterRadar = new BetterRadar(batteryMock,5,new RadarExecutor("diff"));
+        batteryMock = mock(PatriotBattery.class);
+        BetterRadar betterRadar = new BetterRadar(batteryMock,5,new RadarExecutor("same"));
         betterRadar.notice(new Scud());
         verify(batteryMock, times(5)).launchPatriot();
 
