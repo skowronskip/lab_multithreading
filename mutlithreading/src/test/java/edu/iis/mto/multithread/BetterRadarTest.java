3ccb0dd3721101c2ecdee18ef11c581aeadcbe89
Zandros229 <ernest.figura548@gmail.com>
2019-05-07 17:08:06 +0200

    Add BetterRadarTest class

---
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -0,0 +1,26 @@
+package edu.iis.mto.multithread;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+public class BetterRadarTest {
+
+    private static PatriotBattery batteryMock;
+
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
+    @RepeatedTest(1000)
+    public void patriotsShouldBeLaunchedGivenAmountTimes(){
+        batteryMock = mock(PatriotBattery.class);
+        BetterRadar betterRadar = new BetterRadar(batteryMock,new ExecutorForRadar("same"),5);
+        betterRadar.notice(new Scud());
+        verify(batteryMock, times(5)).launchPatriot();
+
+    }
+}
