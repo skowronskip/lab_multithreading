dbe3e2e7a54c15be5f197a1bfbd6ea4e07e146fb
Kaczmarczyk.Michal <209344@edu.p.lodz.pl>
2019-05-09 00:17:00 +0200

    added RepeatRule class and functionality to run test multiple times

--- mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ mutlithreading/src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -1,5 +1,6 @@
 package edu.iis.mto.multithread;
 
+import org.junit.Rule;
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
@@ -8,7 +9,11 @@ import static org.mockito.Mockito.verify;
 
 public class BetterRadarTest {
 
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
     @Test
+    @RepeatRule.Repeat(times = 10)
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
         BetterRadar betterRadar = new BetterRadar(batteryMock, new BatteryExecutor(true), 10);
